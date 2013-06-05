e7fd13630e5852194e68b0bfc4d518de56a74555
Erik <erik.svedang@gmail.com>
2013-06-05 10:50:05 +0200

    fitting image etc

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from PIL import Image, ImageFont, ImageDraw
+from PIL import Image, ImageFont, ImageDraw, ImageOps
 import textwrap
 
 dpi = 300.0
@@ -12,6 +12,9 @@ def cm_to_inches(cm):
 def cm_to_pixels(cm):
 	return inches_to_pixels(cm_to_inches(cm))
 
+def normalized_font_size(points):
+	return int((points / dpi) * 300.0)
+
 a4_size = (cm_to_pixels(21.0), cm_to_pixels(29.7))
 (a4_width, a4_height) = a4_size
 print "A4 pixel dimensions: " + str(a4_size)
@@ -21,44 +24,51 @@ poker_size = (cm_to_pixels(6.3), cm_to_pixels(8.8))
 print "Poker card pixel dimensions: " + str(poker_size)
 
 margin = cm_to_pixels(1.0)
+spacing = cm_to_pixels(0.1)
 
-title_font_path = '/Library/Fonts/Times New Roman.ttf'
-title_font = ImageFont.truetype(title_font_path, 24, encoding='unic')
+border_width = cm_to_pixels(0.1) # around each card
 
-body_font_path = '/Library/Fonts/Times New Roman.ttf'
-body_font = ImageFont.truetype(body_font_path, 16, encoding='unic')
+size_minus_border = (poker_width - (border_width * 2), poker_height - (border_width * 2))
+(width_minus_border, height_minus_border) = size_minus_border
+print "Card size without the borders: " + str(size_minus_border)
 
-def draw_title(card, raw_text):
+font_path = '/Library/Fonts/Times New Roman.ttf'
+title_font = ImageFont.truetype(font_path, normalized_font_size(64), encoding='unic')
+body_font = ImageFont.truetype(font_path, normalized_font_size(46), encoding='unic')
+
+def draw_title(card, raw_text, pos):
 	text = raw_text.decode('utf-8')
-	#(width, height) = font.getsize(text)
 	draw = ImageDraw.Draw(card)
-	draw.text((5, 5), text, font=title_font, fill=(0, 0, 0))
+	draw.text(pos, text, font=title_font, fill=(0, 0, 0))
 
-def draw_body(card, raw_text):
+def draw_body(card, raw_text, y, card_width, chars_per_line):
 	text = raw_text.decode('utf-8')
-	lines = textwrap.wrap(text, width = 40)
-	y_text = 200
-	w = 200
+	lines = textwrap.wrap(text, width = chars_per_line)
 	for line in lines:
-	    width, height = body_font.getsize(line)
+	    line_width, line_height = body_font.getsize(line)
 	    draw = ImageDraw.Draw(card)
-	    draw.text(((w - width) / 2, y_text), line, font = body_font, fill = (0, 0, 0))
-	    y_text += height
+	    draw.text(((card_width - line_width) / 2, y), line, font = body_font, fill = (0, 0, 0))
+	    y += line_height
+
+def draw_illustration(card, card_data, y, vertical_space):
+	illustration = Image.open("illustrations/" + card_data['illustration'] + ".png")
+	cropped = ImageOps.fit(illustration, (width_minus_border, vertical_space), Image.NEAREST, 0.01, (0.5, 0.5)) 
+	card.paste(cropped, (0, y))
 
 def create_card(card_data):
-	illustration = Image.open("illustrations/" + card_data['illustration'] + ".png")
-	card = Image.new("RGB", poker_size, "yellow")
-	card.paste(illustration, (0, 40))
-	draw_title(card, card_data['name'])
-	draw_body(card, card_data['body'])
-	return card
+	card = Image.new("RGB", size_minus_border, "white")
+	draw_illustration(card, card_data, 100, 550)
+	draw_title(card, card_data['name'], (20, 15))
+	draw_body(card, card_data['body'], 750, width_minus_border, 25)
+	card_with_border = ImageOps.expand(card, border=border_width, fill="black")
+	return card_with_border
 
 def create_paper(cards_data_set):
 	cards = [create_card(data) for data in cards_data_set]
 	paper = Image.new("RGB", a4_size, "white")
 	card_index = 0
-	for y in xrange(margin, a4_height, poker_height + margin):
-		for x in xrange(margin, a4_width, poker_width + margin):
+	for y in xrange(margin, a4_height - poker_height, poker_height + spacing):
+		for x in xrange(margin, a4_width - poker_width, poker_width + spacing):
 			card = cards[card_index % len(cards)]
 			card_index += 1
 			paper.paste(card, (x, y))
@@ -66,7 +76,8 @@ def create_paper(cards_data_set):
 
 card_set_1 = [
 	{'illustration': "bison", 'name': "The Death", 'body': "Give each player a warm cup of coffee and a hug."}, 
-	{'illustration': "citrus", 'name': "Lemonade", 'body': "If you're the oldest player at the table, eat a flower."}
+	{'illustration': "citrus", 'name': "Lemonade", 'body': "If you're the oldest player at the table, eat a flower."},
+	{'illustration': "citrus", 'name': "Fish", 'body': "If you're the oldest player at the table, eat a flower."},
 ]
 
 create_paper(card_set_1).show()
