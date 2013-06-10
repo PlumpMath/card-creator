afaa2f1c69baa616e6c3a850f9187d1186e4b966
Erik <erik.svedang@gmail.com>
2013-06-10 17:43:42 +0200

    more illustrations

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 from card_sets import main_set
 from paper import generate_papers_from_set
 
-generate_papers_from_set(main_set, page_indexes = [5,6,7], save = False, show = True)
+generate_papers_from_set(main_set, page_indexes = [5,6,7], save = True, show = True)
