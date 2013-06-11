682f3942b795110e92ac9c601b7ef93f763faf77
Erik <erik.svedang@gmail.com>
2013-06-11 21:47:45 +0200

    balancing the cards

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from stats import print_stats
 
 ALL = range(0, 999)
 
-#generate_pages_from_set(main_set, card_indexes = ALL, save = False, show = False)
+generate_pages_from_set(main_set, card_indexes = [8,13,18,19,24,25,26,27,28,29,30,31], save = True, show = False)
 
-#print_stats(main_set)
+print_stats(main_set)
 
--- stats.py
+++ stats.py
@@ -1,7 +1,9 @@
 def print_stats(card_set):
 	total_count = 0
+	i = 0
 	for card in card_set['cards']:
-		print str(card['count']) + " " + str(card['name']) + " (speed: " + str(card['speed']) + ")"
+		print str(i) + " " + str(card['name']) + " (speed: " + str(card['speed']) + ") count: " + str(card['count'])
 		total_count += card['count']
+		i += 1
 	print "Total count: " + str(total_count)
 	print "Nr of card types: " + str(len(card_set['cards']))
