6aee93c8e0f4103a94ddf47646f66f088aae310f
Erik <erik.svedang@gmail.com>
2014-05-16 09:53:34 +0200

    card tweaks and a few new ones

--- main.py
+++ main.py
@@ -3,9 +3,11 @@ from page import generate_pages_from_set
 from settings import poker_card_settings
 from stats import print_stats
 
-ALL = range(0, 999)
+ALL = range(32, 35)
 
-generate_pages_from_set(main_set, poker_card_settings, card_indexes = ALL, save = False, show = True)
+new = [6, 8, 9, 14, 16, 22, 26, 32, 33, 34, 5]
+
+generate_pages_from_set(main_set, poker_card_settings, card_indexes = new, save = False, show = True)
 
 #print_stats(main_set)
 
